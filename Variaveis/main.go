cca387d7b35c78fa9e5a780a7fb2d87a45096440
Rafael de Figueiredo Alves <58051735+rafael-figueiredo-alves@users.noreply.github.com>
2025-05-09 19:10:41 -0300

    Exemplo com variáveis mais elaborado
    Eu faço uso das variáveis para somar, capturar erros e ainda criei uma função.

--- Variaveis/main.go
+++ Variaveis/main.go
@@ -3,12 +3,36 @@ package main
 import "fmt"
 
 func main() {
-	var a, b int
-	a = 4
-	b = 5
-	fmt.Println("teste ", a, b)
+	var num1, num2 int
+
+	fmt.Println("Informe o primeiro valor a somar")
+	_, err := fmt.Scan(&num1)
+
+	if err != nil {
+		fmt.Println("Valor informado é inválido. Só são aceitos números inteiros")
+		return
+	}
+
+	fmt.Println("Informe o segundo valor a somar")
+	_, err2 := fmt.Scan(&num2)
+
+	if err2 != nil {
+		fmt.Println("Valor informado é inválido. Só são aceitos números inteiros")
+		return
+	}
+
+	fmt.Println()
+
+	fmt.Println("Valores a somar:", num1, "e", num2)
+	fmt.Println("Somando os valores, nós temos o número:", SomarNumeros(num1, num2))
 
 	fmt.Println()
 	fmt.Println("Pressione qualquer tecla para encerrar a aplicação")
-	fmt.Scanln()
+
+	var Fim string
+	fmt.Scanf("%s", &Fim)
+}
+
+func SomarNumeros(primeiroNumero, segundoNumero int) int {
+	return primeiroNumero + segundoNumero
 }
