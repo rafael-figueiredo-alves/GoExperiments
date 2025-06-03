c9701d3a6acf0d14d714b2966abe08f94a68db6a
Rafael de Figueiredo Alves <58051735+rafael-figueiredo-alves@users.noreply.github.com>
2025-06-03 19:05:36 -0300

    Improving API
    - Added functions to be called at endpoints;
    - Added IP;

--- HelloGoFromWeb/main.go
+++ HelloGoFromWeb/main.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"log"
+	"net"
 	"net/http"
 	"os"
 )
@@ -20,42 +21,88 @@ func main() {
 
 	log.Println("Iniciando endpoints")
 
-	http.HandleFunc("GET /clientes", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Exibindo página: %s\n", r.URL.Path)
-	})
+	http.HandleFunc("GET /clientes", GetClientes)
 
-	http.HandleFunc("GET /ping", func(Response http.ResponseWriter, Request *http.Request) {
-		fmt.Fprintf(Response, "Pong")
-	})
+	http.HandleFunc("GET /ping", GetPing)
 
-	http.HandleFunc("POST /user", func(w http.ResponseWriter, r *http.Request) {
-		if r.Method != http.MethodPost {
-			w.WriteHeader(http.StatusMethodNotAllowed)
-			w.Write([]byte(fmt.Sprintf("HTTP method %q not allowed", r.Method)))
-			return
-		}
+	http.HandleFunc("POST /user", PostUser)
 
-		var user = User{
-			Firstname: "Rafael",
-			Lastname:  "Alves",
-			Age:       40,
-		}
+	http.HandleFunc("GET /", GetHome)
 
-		json.NewEncoder(w).Encode(user)
-	})
-
-	http.HandleFunc("GET /", func(Response http.ResponseWriter, Request *http.Request) {
-		if Request.URL.Path != "/" {
-			http.NotFound(Response, Request)
-			return
-		}
-		fmt.Fprintf(Response, "Hello, Go, from web endpoint: %s\n", Request.URL.Path)
-	})
+	exibirIP()
 
 	log.Print("Iniciando servidor")
 
 	if err := http.ListenAndServe(":80", nil); err != nil {
 		fmt.Printf("Erro durante execução do servidor: %v\n", err)
 		os.Exit(1)
+	}
+}
+
+func GetClientes(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Exibindo página: %s\n", r.URL.Path)
+}
+
+func GetPing(Response http.ResponseWriter, Request *http.Request) {
+	fmt.Fprintf(Response, "Pong")
+}
+
+func PostUser(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		w.Write([]byte(fmt.Sprintf("HTTP method %q not allowed", r.Method)))
+		return
+	}
+
+	var user = User{
+		Firstname: "Rafael",
+		Lastname:  "Alves",
+		Age:       40,
+	}
+
+	json.NewEncoder(w).Encode(user)
+}
+
+func GetHome(Response http.ResponseWriter, Request *http.Request) {
+	if Request.URL.Path != "/" {
+		http.NotFound(Response, Request)
+		return
+	}
+	fmt.Fprintf(Response, "Hello, Go, from web endpoint: %s\n", Request.URL.Path)
+}
+
+func exibirIP() {
+	interfaces, err := net.Interfaces()
+	if err != nil {
+		fmt.Println("Erro ao obter interfaces:", err)
+		return
+	}
+
+	for _, iface := range interfaces {
+		// Ignorar interfaces que estão desativadas ou que não suportam endereços
+		if iface.Flags&net.FlagUp == 0 || iface.Flags&net.FlagLoopback != 0 {
+			continue
+		}
+
+		addrs, err := iface.Addrs()
+		if err != nil {
+			fmt.Printf("Erro ao obter endereços da interface %s: %v\n", iface.Name, err)
+			continue
+		}
+
+		for _, addr := range addrs {
+			var ip net.IP
+			switch v := addr.(type) {
+			case *net.IPNet:
+				ip = v.IP
+			case *net.IPAddr:
+				ip = v.IP
+			}
+
+			// Exibir apenas endereços IPv4
+			if ip != nil && ip.To4() != nil {
+				fmt.Printf("Interface: %s, IP: %s\n", iface.Name, ip.String())
+			}
+		}
 	}
 }
